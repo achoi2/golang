34de577b527af7ef741e0acf7243f6e7ab59e161
Andrew <andrewchoi1@hotmail.com>
2019-02-12 20:00:33 -0500

    go files

---
+++ cards/deck.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Jack", "Queen", "King", "Ace"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
